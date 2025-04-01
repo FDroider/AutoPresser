4e46d8b7506c077cd0d9debc2d3006736c73d8e4
Droid-Android <nikolajtupalo@gmail.com>
2025-04-01 12:16:20 +0300

    update method start_script

--- UI/main_screen.py
+++ UI/main_screen.py
@@ -63,6 +63,19 @@ class MainScreen(QtWidgets.QWidget):
         self.dropdown_script.addItems(["Presser", "AutoClicker"])
         self.dropdown_script.setCurrentIndex(0)
 
+        self.dlg_preset = QtWidgets.QDialog()
+        self.dlg_preset.setWindowTitle("Save preset")
+        self.dlg_filename = QtWidgets.QTextEdit()
+        self.dlg_filename.setPlaceholderText("File name")
+        self.dlg_filename.setFixedSize(QtCore.QSize(200, 35))
+        self.dlg_btn_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.StandardButton.Ok |
+                                             QtWidgets.QDialogButtonBox.StandardButton.Cancel)
+        self.dlg_btn_box.accepted.connect(
+            lambda: self.save_preset(self.dlg_filename.toPlainText()) if self.dlg_filename.toPlainText().replace(" ", "") != ""
+            else self.master.show_err("ValueError", "Enter file name!"))
+        self.dlg_btn_box.rejected.connect(self.dlg_preset.close)
+        self.dlg_preset.setLayout(self.master.create_layout("v", widgets=[self.dlg_filename, self.dlg_btn_box]))
+
         self.group_btn = self.master.create_layout(widgets=[self.btn_settings, self.btn_save, self.btn_save_presets])
         self.key_layout = self.master.create_layout(widgets=[self.dropdown_btn, self.entry])
         self.chose_script_l = self.master.create_layout(widgets=[self.btn_start, self.dropdown_script])
@@ -74,14 +87,14 @@ class MainScreen(QtWidgets.QWidget):
         self.tab_2.setLayout(self.hot_key_frame.layout())
 
     def get_options_one_key(self):
-        return self.master.settings.settings_script_frame.duration_one_key.toPlainText()
+        return self.master.settings.get_script_settings()[0]
 
     def get_options_hot_key(self):
-        return (self.master.settings.settings_script_frame.delay_hot_key.toPlainText(),
-                self.master.settings.settings_script_frame.interval_hot_key.toPlainText())
+        return self.master.settings.get_script_settings()[1:]
 
     def open_settings(self):
         self.master.setWindowTitle("Settings")
+        self.master.settings.settings_script_frame.update_list_windows()
         self.master.stack_widget.setCurrentIndex(1)
 
     def change_dropdown_btn(self):
@@ -93,24 +106,27 @@ class MainScreen(QtWidgets.QWidget):
     def start_script(self):
         if self.dropdown_btn.currentIndex() == 3:
             if self.entry.toPlainText().lower().replace(" ", "") == "":
-                return
+                return False
         if self.tab_widget.currentIndex() == 0:
             if self.one_key_frame.entry.toPlainText().replace(" ", "") == "":
-                return
+                return False
         else:
             if self.hot_key_frame.entry.toPlainText().replace(" ", "") == "":
-                return
+                return False
 
-        self.dropdown_script.setDisabled(True)
+        w_name = self.master.settings.get_select_window(False)
+
         self.starter_script = self.starter_script_thread(self, self.tab_widget.currentIndex(),
-                                                         self.dropdown_script.currentIndex())
+                                                         self.dropdown_script.currentIndex(),
+                                                         w_name)
+
         self.starter_script.start()
         self.dropdown_script.setDisabled(True)
         self.tab_widget.setDisabled(True)
         self.btn_start.setText("Stop script")
         self.btn_start.clicked.disconnect()
         self.btn_start.clicked.connect(self.stop_script)
-        self.master.master.tray.setIcon(self.master.master.tray.active_icon)
+        self.app.tray.setIcon(self.app.tray.active_icon)
         return True
 
     def stop_script(self):
@@ -120,52 +136,42 @@ class MainScreen(QtWidgets.QWidget):
         self.btn_start.setText("Start script")
         self.btn_start.clicked.disconnect()
         self.btn_start.clicked.connect(self.start_script)
-        self.master.master.tray.setIcon(self.master.master.tray.default_icon)
+        self.app.tray.setIcon(self.app.tray.default_icon)
 
     def save_settings_script(self):
         if not exists("DataSave"):
             mkdir("DataSave")
+        script_settings = self.master.settings.get_script_settings()
         settings = {"bind_one_key": self.one_key_frame.get_value_entry(),
                     "bind_hot_key": f"{self.hot_key_frame.get_extra_key()}+{self.hot_key_frame.get_value_entry()}",
                     "button": self.dropdown_btn.currentText(),
                     "button_key": self.entry.toPlainText(),
-                    "one_key_duration": self.master.settings.settings_script_frame.duration_one_key.toPlainText(),
-                    "hot_key_delay": self.master.settings.settings_script_frame.delay_hot_key.toPlainText(),
-                    "hot_key_interval": self.master.settings.settings_script_frame.interval_hot_key.toPlainText()}
+                    "one_key_duration": script_settings[0],
+                    "hot_key_delay": script_settings[1],
+                    "hot_key_interval": script_settings[2]}
         try:
             with open("DataSave/config_script.json", "w") as f:
                 f.write(dumps(settings))
             self.master.show_info("Success", "Settings saved")
+            return True
         except Exception as e:
             self.master.show_err("Exception", str(e))
+            return False
 
     def open_save_dlg(self):
-        dlg = QtWidgets.QDialog()
-        dlg.setWindowTitle("Save preset")
-        layout = QtWidgets.QVBoxLayout()
-        filename = QtWidgets.QTextEdit()
-        filename.setPlaceholderText("File name")
-        filename.setFixedSize(QtCore.QSize(200, 30))
-        btn_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.StandardButton.Ok |
-                                             QtWidgets.QDialogButtonBox.StandardButton.Cancel)
-        btn_box.accepted.connect(lambda: self.save_preset(filename.toPlainText()) if filename.toPlainText().replace(" ", "") != ""
-                                 else self.master.show_err("ValueError", "Enter file name!"))
-        btn_box.rejected.connect(dlg.close)
-        layout.addWidget(filename)
-        layout.addWidget(btn_box)
-        dlg.setLayout(layout)
-        dlg.exec()
+        self.dlg_preset.exec()
 
     def save_preset(self, filename):
         if not exists("DataSave/Presets"):
             makedirs("DataSave/Presets")
+        script_settings = self.master.settings.get_script_settings()
         settings = {"bind_one_key": self.one_key_frame.get_value_entry(),
                     "bind_hot_key": f"{self.hot_key_frame.get_extra_key()}+{self.hot_key_frame.get_value_entry()}",
                     "button": self.dropdown_btn.currentText(),
                     "button_key": self.entry.toPlainText(),
-                    "one_key_dur": self.master.settings.settings_script_frame.duration_one_key.toPlainText(),
-                    "hot_key_delay": self.master.settings.settings_script_frame.delay_hot_key.toPlainText(),
-                    "hot_key_interval": self.master.settings.settings_script_frame.interval_hot_key.toPlainText()}
+                    "one_key_dur": script_settings[0],
+                    "hot_key_delay": script_settings[1],
+                    "hot_key_interval": script_settings[2]}
         try:
             with open(f"DataSave/Presets/{filename}.json", "w") as f:
                 f.write(dumps(settings))
@@ -201,23 +207,14 @@ class MainScreen(QtWidgets.QWidget):
                         else:
                             if i[0:7] == "one_key":
                                 if i[8:] == "duration":
-                                    self.master.settings.settings_script_frame.duration_one_key.setText(
-                                        settings[i])
-                                    self.master.settings.settings_script_frame.duration_one_key.setAlignment(
-                                        QtGui.Qt.AlignmentFlag.AlignCenter)
+                                    self.master.settings.set_script_settings(settings[i])
                                 continue
                             elif i[0:7] == "hot_key":
                                 match i[8:]:
                                     case "delay":
-                                        self.master.settings.settings_script_frame.delay_hot_key.setText(
-                                            settings[i])
-                                        self.master.settings.settings_script_frame.delay_hot_key.setAlignment(
-                                            QtGui.Qt.AlignmentFlag.AlignCenter)
+                                        self.master.settings.set_script_settings(delay_hot_key=settings[i])
                                     case "interval":
-                                        self.master.settings.settings_script_frame.interval_hot_key.setText(
-                                            settings[i])
-                                        self.master.settings.settings_script_frame.interval_hot_key.setAlignment(
-                                            QtGui.Qt.AlignmentFlag.AlignCenter)
+                                        self.master.settings.set_script_settings(interval_hot_key=settings[i])
                                 continue
                             else:
                                 if i == "button":
