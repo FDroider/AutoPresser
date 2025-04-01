553ceeed463a381f386e218a980006911e5e7e0f
Droid-Android <nikolajtupalo@gmail.com>
2025-04-01 12:22:34 +0300

    added classes for send click

--- auto_clicker.py
+++ auto_clicker.py
@@ -3,13 +3,20 @@ from pyautogui import PyAutoGUIException
 from pynput.keyboard import Listener, KeyCode, HotKey
 from PySide6.QtCore import QThread
 from time import sleep
-
+from subprocess import check_output, run
+from platform import system
+try:
+    import win32gui
+    import win32con
+    import win32api
+    from pygetwindow import getWindowsWithTitle
+except:
+    pass
 
 class AutoClicker(QThread):
-    def __init__(self, button: str, delay: float, interval: float):
+    def __init__(self, button: str, delay: float):
         super().__init__()
         self.delay = float(delay)
-        self.interval = float(interval)
         self.btn_click = button.lower()
         self.running = False
         self.work_program = True
@@ -28,18 +35,82 @@ class AutoClicker(QThread):
         while self.work_program:
             while self.running:
                 try:
-                    click(button=self.btn_click, interval=self.interval)
+                    click(self.btn_click)
                 except PyAutoGUIException:
                     press(self.btn_click)
             sleep(self.delay)
 
+class AutoClickerWindowLinux(AutoClicker):
+    def __init__(self, button: str, delay: float, window_name: str):
+        super().__init__(button, delay)
+        self.window_id = check_output(["xdotool", "search", "--name", f"{window_name}"]).split()[0].decode()
+        self.check_button()
+
+    def check_button(self):
+        if self.btn_click == "left":
+            self.btn_click = 1
+        elif self.btn_click == "right":
+            self.btn_click = 3
+        elif self.btn_click == "middle":
+            self.btn_click = 2
+
+    def run(self):
+        while self.work_program:
+            while self.running:
+                if self.btn_click in (1, 2, 3):
+                    run(["xdotool", "click", "--window", f"{self.window_id}", f"{self.btn_click}"])
+                else:
+                    run(["xdotool", "key", "--window", f"{self.window_id}", f"{self.btn_click}"])
+            sleep(self.delay)
+
+class AutoClickerWindow(AutoClicker):
+    def __init__(self, button: str, delay: float, window_name: str):
+        super().__init__(button, delay)
+        self.window_handel = win32gui.FindWindow(None, window_name[:-4])
+        if self.window_handel in (0, "0") or self.window_handel is None:
+            self.window_handel = win32gui.FindWindow(None, getWindowsWithTitle(window_name[:-4])[0].title)
+        rect = win32gui.GetWindowRect(self.window_handel)
+        self.lParam = win32api.MAKELONG(rect[0]+100, rect[1]+100)
+
+    def send_click(self, button_down, button_up, key=None):
+        if key is not None:
+            win32api.PostMessage(self.window_handel, win32con.WM_KEYDOWN, hex(win32api.VkKeyScan(key)),
+                                 self.lParam)
+            win32api.PostMessage(self.window_handel, win32con.WM_KEYUP, hex(win32api.VkKeyScan(key)),
+                                 self.lParam)
+            return
+        win32api.PostMessage(self.window_handel, button_down, 1, self.lParam)
+        win32api.PostMessage(self.window_handel, button_up, 0, self.lParam)
+
+
+    def run(self):
+        while self.work_program:
+            while self.running:
+                if self.btn_click == "left":
+                    self.send_click(win32con.WM_LBUTTONDOWN, win32con.WM_LBUTTONUP)
+                elif self.btn_click == "right":
+                    self.send_click(win32con.WM_RBUTTONDOWN, win32con.WM_RBUTTONUP)
+                elif self.btn_click == "middle":
+                    self.send_click(win32con.WM_MBUTTONDOWN, win32con.WM_MBUTTONUP)
+                else:
+                    self.send_click(None, None, self.btn_click)
+                sleep(0.2)
+            sleep(self.delay)
 
 async def start_one_key(start_key, button, *args):
     global app, listener
 
     delay = 0.0 if args[0] == "" or float(args[0]) < 0.0 else float(args[0])
+    w_name = args[1].rstrip() if args[1] else "auto"
 
-    app = AutoClicker(button, delay, 0)
+    main_system = system().lower()
+
+    if w_name != "auto" and main_system == "windows":
+        app = AutoClickerWindow(button, delay, w_name)
+    elif w_name != "auto" and main_system == "linux":
+        app = AutoClickerWindowLinux(button, delay, w_name)
+    else:
+        app = AutoClicker(button, delay)
     app.start()
 
     def on_press(key):
@@ -57,14 +128,20 @@ async def start_two_keys(start_key, start_two_key, button, *args):
     global app, listener
 
     delay = 0.0 if args[0][0] == "" or float(args[0][0]) < 0.0 else float(args[0][0])
-    interval = 0.0 if args[0][1] == "" or float(args[0][1]) < 0.0 else float(args[0][1])
+    w_name = args[1].rstrip() if args[1] else "auto"
 
-    app = AutoClicker(button, delay, interval)
+    main_system = system().lower()
+
+    if w_name != "auto" and main_system == "windows":
+        app = AutoClickerWindow(button, delay, w_name)
+    elif w_name != "auto" and main_system == "linux":
+        app = AutoClickerWindowLinux(button, delay, w_name)
+    else:
+        app = AutoClicker(button, delay)
     app.start()
-    click = False
 
     def on_active():
-        if click:
+        if not app.running:
             app.start_clicking()
         else:
             app.stop_clicking()
@@ -75,7 +152,7 @@ async def start_two_keys(start_key, start_two_key, button, *args):
     def on_press(f):
         return lambda k: f(listener.canonical(k))
 
-    with Listener(on_press=on_press(hotkey.press), on_release=on_press(hotkey.release)) as listener:
+    with Listener(on_press=on_press(hotkey.press)) as listener:
         listener.join()
 
 
--- press_click.py
+++ press_click.py
@@ -3,17 +3,25 @@ from pynput.keyboard import Listener, KeyCode, HotKey
 from pyautogui import mouseDown, mouseUp
 from pyautogui import keyUp, keyDown
 from pyautogui import PyAutoGUIException
+from subprocess import run, check_output
+from platform import system
+try:
+    import win32gui
+    import win32con
+    import win32api
+    from pygetwindow import getWindowsWithTitle
+except:
+    pass
 
 class MouseControl:
     def __init__(self, button: str, duration: float = 0.0):
-        super().__init__()
         self.btn_click = button.lower()
-        if button.lower() in ("shift", "alt", "ctrl"):
+        if self.btn_click in ("shift", "alt", "ctrl"):
             self.btn_click = f"{button.lower()}left"
         self.duration = duration
         self.mouse_pressed = False
 
-    def start_mouse_down(self):
+    def btn_down(self):
         self.mouse_pressed = True
         try:
             mouseDown(button=self.btn_click)
@@ -21,13 +29,9 @@ class MouseControl:
             keyDown(self.btn_click)
         if self.duration != 0.0:
             sleep(self.duration)
-            try:
-                mouseUp(self.btn_click)
-            except PyAutoGUIException:
-                keyUp(self.btn_click)
-            self.mouse_pressed = False
+            self.btn_up()
 
-    def stop_mouse_down(self):
+    def btn_up(self):
         self.mouse_pressed = False
         try:
             mouseUp(button=self.btn_click)
@@ -36,17 +40,101 @@ class MouseControl:
 
     def start(self):
         if self.mouse_pressed is True:
-            self.stop_mouse_down()
+            self.btn_up()
         else:
-            self.start_mouse_down()
+            self.btn_down()
+
+    def stop(self):
+        self.btn_up()
+
+
+class MouseControlWindowLinux(MouseControl):
+    def __init__(self, button: str, duration: float, window_name: str):
+        super().__init__(button, duration)
+        self.window_id = check_output(["xdotool", "search", "--name", f"{window_name}"]).split()[0].decode()
+        self.check_button()
+
+    def check_button(self):
+        if self.btn_click == "left":
+            self.btn_click = 1
+        elif self.btn_click == "right":
+            self.btn_click = 3
+        elif self.btn_click == "middle":
+            self.btn_click = 2
+
+    def btn_down(self):
+        self.mouse_pressed = True
+        try:
+            if self.btn_click in ("left", "right", "middle"):
+                    run(["xdotool", "mousedown", f"--window", f"{self.window_id}", f"{self.btn_click}"])
+            else:
+                    run(["xdotool", "keydown", f"--window", f"{self.window_id}", f"{self.btn_click}"])
+        except FileNotFoundError:
+            raise FileNotFoundError("Not found xdotool. Please install it for correct work app")
+        if self.duration != 0.0:
+            sleep(self.duration)
+            self.btn_up()
+
+    def btn_up(self):
+        self.mouse_pressed = False
+        try:
+            if self.btn_click in ("left", "right", "middle"):
+                    run(["xdotool", "mouseup", f"--window", f"{self.window_id}", f"{self.btn_click}"])
+            else:
+                    run(["xdotool", "keyup", f"--window", f"{self.window_id}", f"{self.btn_click}"])
+        except FileNotFoundError:
+            raise FileNotFoundError("Not found xdotool. Please install it for correct to work app")
+
+
+class MouseControlWindow(MouseControl):
+    def __init__(self, button, duration: float, window_name: str):
+        super().__init__(button, duration)
+        self.window_handel = win32gui.FindWindow(None, window_name[:-4])
+        if self.window_handel in (0, "0") or self.window_handel is None:
+            self.window_handle = win32gui.FindWindow(None, getWindowsWithTitle(window_name[:-4])[0].title)
+        rect = win32gui.GetWindowRect(self.window_handle)
+        self.lParam = win32api.MAKELONG(rect[0] + 100, rect[1] + 100)
+
+    def btn_down(self):
+        if self.btn_click == "left":
+            win32api.PostMessage(self.window_handel, win32con.WM_LBUTTONDOWN, 1, self.lParam)
+        elif self.btn_click == "right":
+            win32api.PostMessage(self.window_handel, win32con.WM_RBUTTONDOWN, 1, self.lParam)
+        elif self.btn_click == "middle":
+            win32api.PostMessage(self.window_handel, win32con.WM_MBUTTONDOWN, 1, self.lParam)
+        else:
+            win32api.PostMessage(self.window_handel, win32con.WM_KEYDOWN, hex(win32api.VkKeyScan(self.btn_click)),
+                                 self.lParam)
+        if self.duration != 0.0:
+            sleep(self.duration)
+            self.btn_up()
+
+    def btn_up(self):
+        if self.btn_click == "left":
+            win32api.PostMessage(self.window_handel, win32con.WM_LBUTTONUP, 0, self.lParam)
+        elif self.btn_click == "right":
+            win32api.PostMessage(self.window_handel, win32con.WM_RBUTTONUP, 0, self.lParam)
+        elif self.btn_click == "middle":
+            win32api.PostMessage(self.window_handel, win32con.WM_MBUTTONUP, 0, self.lParam)
+        else:
+            win32api.PostMessage(self.window_handel, win32con.WM_KEYUP, hex(win32api.VkKeyScan(self.btn_click)),
+                                 self.lParam)
 
 
 async def start_one_key(start_key, button, *args):
     global app, listener
 
     duration = 0.0 if args[0] == "" or float(args[0]) < 0.0 else float(args[0])
+    w_name = args[1].rstrip() if args[1] else "auto"
 
-    app = MouseControl(button, duration)
+    main_system = system().lower()
+
+    if w_name != "auto" and main_system == "windows":
+        app = MouseControlWindow(button, duration, w_name)
+    elif w_name != "auto" and main_system == "linux":
+        app = MouseControlWindowLinux(button, duration, w_name)
+    else:
+        app = MouseControl(button, duration)
 
     def on_press(key):
         if key == KeyCode(char=start_key):
@@ -60,8 +148,16 @@ async def start_two_keys(start_key, start_two_key, button, *args):
     global app, listener
 
     duration = 0.0 if args[0][0] == "" or float(args[0][0]) < 0.0 else float(args[0][0])
+    w_name = args[1] if args[1] else "auto"
 
-    app = MouseControl(button, duration)
+    main_system = system().lower()
+
+    if w_name != "auto" and main_system == "windows":
+        app = MouseControlWindow(button, duration, w_name)
+    elif w_name != "auto" and main_system == "linux":
+        app = MouseControlWindowLinux(button, duration, w_name)
+    else:
+        app = MouseControl(button, duration)
 
     hotkey = HotKey((HotKey.parse(f"<{start_key}>+{start_two_key}")),
                     on_activate=app.start)
@@ -69,10 +165,10 @@ async def start_two_keys(start_key, start_two_key, button, *args):
     def on_press(f):
         return lambda k: f(listener.canonical(k))
 
-    with Listener(on_press=on_press(hotkey.press), on_release=on_press(hotkey.release)) as listener:
+    with Listener(on_press=on_press(hotkey.press)) as listener:
         listener.join()
 
 
 async def stop():
-    app.stop_mouse_down()
+    app.stop()
     listener.stop()
